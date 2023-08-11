994161db7f9e092301247da97e4b88ac0393ef40
Afonso Bernardes <84087794+AfonsoBernardes@users.noreply.github.com>
2023-08-11 19:15:02 +0000

    Separated code into fucntions for easiers readibility and maintenance.

--- src/app.js
+++ src/app.js
@@ -10,19 +10,24 @@ const adjective = ["great", "big"];
 const noun = ["jogger", "racoon", "pepe"];
 const extensions = [".com", ".net", ".us", ".io", ".racoon", ".pe"];
 
+function checkNameInExtensions(name, extensions) {
+  if (extensions.includes(`.${name}`)) {
+    domains += `${pron}${adj}.${name}<br>`;
+  } else {
+    extensions.forEach(ext => {
+      domains += `${pron}${adj}${name}${ext}<br>`;
+    });
+  }
+  return domains;
+}
+
 window.onload = function() {
   //write your code here
   let domains = "";
   pronoun.forEach(pron => {
     adjective.forEach(adj => {
       noun.forEach(name => {
-        if (extensions.includes(`.${name}`)) {
-          domains += `${pron}${adj}.${name}<br>`;
-        } else {
-          extensions.forEach(ext => {
-            domains += `${pron}${adj}${name}${ext}<br>`;
-          });
-        }
+        domains = checkNameInExtensions(name, extensions);
       });
     });
   });
