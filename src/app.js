0108f21d93950868512190b94c9d7b88b40fa19b
Afonso Bernardes <84087794+AfonsoBernardes@users.noreply.github.com>
2023-08-11 18:55:00 +0000

    Basic project done.

--- src/app.js
+++ src/app.js
@@ -5,7 +5,22 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
+const pronoun = ["the", "our"];
+const adjective = ["great", "big"];
+const noun = ["jogger", "racoon"];
+const extensions = [".com", ".net", ".us", ".io"];
+
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let domains = "";
+  pronoun.forEach(pron => {
+    adjective.forEach(adj => {
+      noun.forEach(name => {
+        extensions.forEach(ext => {
+          domains += `${pron}${adj}${name}${ext}<br>`;
+        });
+      });
+    });
+  });
+  document.getElementById("domain").innerHTML = domains;
 };
